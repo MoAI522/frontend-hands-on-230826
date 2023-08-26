7ef1f1300102bea3b30478aed3022ea4ae6cc9a8
MoAI522 <urotanre522@gmail.com>
2023-08-26 20:15:26 +0900

    シード作成

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times.each do |i|
+  Trainer.new(name: "trainer#{i}").save
+end
+
+monster_species = %w[スナギズモ ガマグチ ボクスミス ワシリバシ ドマーチュ ハルチュウ ポゼッセン]
+monster_species.each do |name|
+  MonsterSpecies.new(name:).save
+end
+
+# クソ汚い
+c = 0
+Trainer.all.each do |t|
+  3.times.each do |i|
+    Monster.new(name: "モン#{t.id}-#{i}", level: (i + 1), monster_species_id: (c + 1), owner_id: t.id).save
+    c = (c + 1) % monster_species.length
+  end
+end
